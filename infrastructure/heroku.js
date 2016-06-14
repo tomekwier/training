97c3647e7b93757dcc552e3dc492a7d43a254dc6
Tomek Wiercioch <tomasz.wiercioch@motorolasolutions.com>
2016-06-14 10:41:55 +0200

    Add configurator

--- infrastructure/heroku.js
+++ infrastructure/heroku.js
@@ -2,6 +2,27 @@ var heroin = require('heroin-js');
 
 var configurator = heroin(process.env.HEROKU_API_TOKEN);
 
-configurator.export('training-tomek').then((result) => {
-    console.log(result);
-});
+// configurator.export('training-tomek').then((result) => {
+//     console.log(result);
+// });
+
+var prod = { name: 'training-tomek',
+    region: 'eu',
+    maintenance: false,
+    stack: 'cedar-14',
+    config_vars: {NODE_ENV: 'production'},
+    addons: { mongolab: { plan: 'mongolab:sandbox' } },
+    collaborators: [ 'tomasz.wiercioch@gmail.com' ],
+    features:
+    { 'runtime-dyno-metadata': { enabled: false },
+        'log-runtime-metrics': { enabled: false },
+        'http-session-affinity': { enabled: false },
+        preboot: { enabled: false },
+        'http-shard-header': { enabled: false },
+        'http-end-to-end-continue': { enabled: false },
+        'http-sni': { enabled: false } },
+    formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+    log_drains: []
+};
+
+configurator(prod);
